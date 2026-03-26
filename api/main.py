89e9d947ee18c6b8fe42fafb06c4a16420d20331
Anandkumar-Kambar <anandkumarkambar@gmail.com>
2026-03-26 13:02:29 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -321,6 +321,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "api.main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", "8000")),
+        port=int(os.getenv("PORT", "10000")),
         reload=True,
     )
